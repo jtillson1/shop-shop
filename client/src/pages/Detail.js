725f3173d38ad741f71ed20cf3a3968906a5dd2b
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-10-30 16:05:35 -0500

    shopping cart persists for each component, and updates that accordingly

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -23,7 +23,7 @@ function Detail() {
   const { products, cart } = state;
 
   const addToCart = () => {
-    const itemInCart = cart.find((CartItem) => CartItem._id === id);
+    const itemInCart = cart.find((cartItem) => cartItem._id === id)
 
     if (itemInCart) {
       dispatch({
@@ -31,32 +31,42 @@ function Detail() {
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
+      // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
       dispatch({
         type: ADD_TO_CART,
-        product: {...currentProduct, purchaseQuantity: 1 }
+        product: { ...currentProduct, purchaseQuantity: 1 }
       });
+      // if product isn't in the cart yet, add it to the current shopping cart in IndexedDB
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
-  };
-  
+  }
+
   const removeFromCart = () => {
     dispatch({
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+
+    // upon removal from cart, delete the item from IndexedDB using the `currentProduct._id` to locate what to remove
+    idbPromise('cart', 'delete', { ...currentProduct });
   };
   useEffect(() => {
     // already in global store
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
-    } 
+    }
     // retrieved from server
     else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
-  
+
       data.products.forEach((product) => {
         idbPromise('products', 'put', product);
       });
@@ -71,7 +81,7 @@ function Detail() {
       });
     }
   }, [products, data, loading, dispatch, id]);
-  
+
 
   return (
     <>
@@ -95,7 +105,7 @@ function Detail() {
               Add to Cart
             </button>
             <button disabled={!cart.find(p => p._id === currentProduct._id)}
-             onClick={removeFromCart}>
+              onClick={removeFromCart}>
               Remove from Cart
             </button>
           </p>
