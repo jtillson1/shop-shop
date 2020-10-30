7e881ed9817537b335ef85b82e5f7283ec155d00
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-10-30 13:52:41 -0500

    add to cart functioning

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-
+import { useStoreContext } from '../../utils/GlobalState';
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 function ProductItem(item) {
   const {
     image,
@@ -10,7 +11,14 @@ function ProductItem(item) {
     price,
     quantity
   } = item;
+  const [state, dispatch] = useStoreContext();
 
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...item, purchaseQuantity: 1 }
+    });
+  };
   return (
     <div className="card px-1 py-1">
       <Link to={`/products/${_id}`}>
@@ -24,7 +32,7 @@ function ProductItem(item) {
         <div>{quantity} {pluralize("item", quantity)} in stock</div>
         <span>${price}</span>
       </div>
-      <button>Add to cart</button>
+      <button onClick={addToCart}>Add to cart</button>
     </div>
   );
 }
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,20 +2,28 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 import Cart from "../components/Cart";
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
 function Detail() {
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
-  
   const [currentProduct, setCurrentProduct] = useState({})
-  
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-  
   const { products } = state;
-  
+
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  };
   useEffect(() => {
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
@@ -45,7 +53,7 @@ function Detail() {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
-            <button>
+            <button onClick={addToCart}>
               Add to Cart
             </button>
             <button>
