8a9f4089f7ba3ca1d3bb91b1935f0e4e9555dfea
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-10-30 14:10:59 -0500

    remove from cart is functioning

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -5,23 +5,42 @@ import { useStoreContext } from "../utils/GlobalState";
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 import Cart from "../components/Cart";
+import CartItem from "../components/CartItem";
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
   ADD_TO_CART,
   UPDATE_PRODUCTS,
 } from '../utils/actions';
+
 function Detail() {
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
   const [currentProduct, setCurrentProduct] = useState({})
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-  const { products } = state;
+  const { products, cart } = state;
 
   const addToCart = () => {
+    const itemInCart = cart.find((CartItem) => CartItem._id === id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: {...currentProduct, purchaseQuantity: 1 }
+      });
+    }
+  };
+  
+  const removeFromCart = () => {
     dispatch({
-      type: ADD_TO_CART,
-      product: { ...currentProduct, purchaseQuantity: 1 }
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
     });
   };
   useEffect(() => {
@@ -56,7 +75,8 @@ function Detail() {
             <button onClick={addToCart}>
               Add to Cart
             </button>
-            <button>
+            <button disabled={!cart.find(p => p._id === currentProduct._id)}
+             onClick={removeFromCart}>
               Remove from Cart
             </button>
           </p>
