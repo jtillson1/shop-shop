faf20b59b3f8e8a9b0e44942833476b43b89ab67
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-10-30 15:15:39 -0500

    use Effects updated and data persisting in console

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -6,6 +6,8 @@ import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 import Cart from "../components/Cart";
 import CartItem from "../components/CartItem";
+import { idbPromise } from "../utils/helpers";
+
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -44,15 +46,32 @@ function Detail() {
     });
   };
   useEffect(() => {
+    // already in global store
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
-    } else if (data) {
+    } 
+    // retrieved from server
+    else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+  
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [products, data, dispatch, id]);
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
+  
 
   return (
     <>
